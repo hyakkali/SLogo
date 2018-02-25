5a92b9d5f4d8518be86e19f5193fa5cea6b99faa
Hemanth Yakkali <hy115@duke.edu>
2018-02-24 21:09:56 -0500

    Added block comments to describe private variables and methods

--- src/Turtle.java
+++ src/Turtle.java
@@ -8,54 +8,116 @@ import javafx.scene.image.ImageView;
  */
 public class Turtle extends ImageView{
 	
+	/**
+	 * X coordinate of the turtle
+	 */
 	private double xLocation;
+	
+	/**
+	 * Y coordinate of the turtle
+	 */
 	private double yLocation;
-	private double angle;
+	
+	/**
+	 * Current heading of the turtle
+	 */
+	private double heading;
+	
+	/**
+	 * String of path to an image file
+	 */
 	private String imageURL;
 	
+	/**
+	 * Image object of the turtle
+	 */
+	private Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(imageURL));
+	
+	/**
+	 * Turtle constructor that sets X and Y coordinates and heading to 0, sets 
+	 * image of the turtle to the default image.
+	 */
 	public Turtle(){
 		super(); //sets image found in url
 		this.xLocation = 0;
 		this.yLocation = 0;
-		this.angle = 0;
-//		this.imageURL; 
+		this.heading = 0;
+		this.setImage(turtleImage);
 	}
 	
+	/**
+	 * 
+	 * @param angle Heading of the turtle
+	 * @param amount Amount of pixels to move
+	 */
 	public void move(double angle, double amount) {
-		xLocation += calculateXAmount(angle, amount);
-		yLocation += calculateYAmount(angle, amount);
+		this.xLocation += calculateXAmount(angle, amount);
+		this.yLocation += calculateYAmount(angle, amount);
 	}
 	
+	/**
+	 * 
+	 * @param xCoordinate X coordinate of the turtle
+	 */
 	//setters
 	public void setXPosition(double xCoordinate) {
-		xLocation = xCoordinate;
+		this.xLocation = xCoordinate;
 	}
 	
+	/**
+	 * 
+	 * @param yCoordinate Y coordinate of the turtle
+	 */
 	public void setYPosition(double yCoordinate) {
-		yLocation = yCoordinate;
+		this.yLocation = yCoordinate;
 	}
 	
+	/**
+	 * 
+	 * @param heading Heading of the turtle
+	 */
 	public void setAngle(double heading) {
-		angle = heading;
+		this.heading = heading;
 	}
 	
-	public void setImageURL(String url) {
+	/**
+	 * 
+	 * @param url String path to an image
+	 */
+	public void setImage(String url) {
 		imageURL = url;
+		this.setImage(turtleImage);
 	}
-		
+	
 	//getters
+	/**
+	 * 
+	 * @return X coordinate of the turtle
+	 */
 	public double getXLocation() {
 		return xLocation;
 	}
 	
+	/**
+	 * 
+	 * @return Y coordinate of the turtle
+	 */
 	public double getYLocation() {
 		return yLocation;
 	}
 	
+	/**
+	 * 
+	 * @return Current heading of the turtle
+	 */
 	public double getAngle() {
-		return angle;
+		return heading;
 	}
 	
+	/**
+	 * 
+	 * @return Current path to the image file being used 
+	 */
 	public String getImageURL() {
 		return imageURL;
 	}
