d7ab7866b738507c0712d2cd3f07f07234277199
Hemanth Yakkali <hy115@duke.edu>
2018-02-25 22:50:40 -0500

    Instantiated turtle, interface, and controller

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,16 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import turtle.Turtle;
 
 public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		
+		Turtle turtle = new Turtle();
+		UserInterface view =  new UserInterface();
+		Controller controller = new TurtleViewController(view, turtle);
 	}
 	
 	public static void main(String[] args) {
