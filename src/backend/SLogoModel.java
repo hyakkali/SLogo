9d7da537a89eae79ad1624473684fb8609a7c65d
Alexi Kontos <aak29@duke.edu>
2018-02-27 19:30:37 -0500

    bug fixing

--- src/backend/SLogoModel.java
+++ src/backend/SLogoModel.java
@@ -7,26 +7,31 @@ import resources.constants.Constants;
 import resources.languages.Language;
 import resources.languages.LanguageFactory;
 import main.Controller;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Stack;
 
 public class SLogoModel {
 
     SLogoData myData;
+    Controller myController;
     private Executor myExecutor;
     private CommandFactory myFactory;
-    private Controller myController;
     private LanguageFactory myLanguageFactory;
+
 
     public SLogoModel(Controller ctrl) {
         myFactory = new CommandFactory();
-        registerCommands(myFactory);
-        myExecutor = new Executor(ctrl, myFactory);
         myLanguageFactory = new LanguageFactory();
+        registerCommands(myFactory, myLanguageFactory);
+        myExecutor = new Executor(ctrl, myFactory);
+
     }
 
-    private void registerCommands(CommandFactory cmdFact) {
+    private void registerCommands(CommandFactory cmdFact, LanguageFactory langFact) {
+        Set<String> keySet = new HashSet(langFact.getLanguage("English").getKeys());
         try {
-            for (String key : myLanguageFactory.getLanguage("English").getKeys()) {
+            for (String key : keySet) {
                 Class<?> regClass = Class.forName(key);
                 cmdFact.registerCommand(key, regClass);
             }
